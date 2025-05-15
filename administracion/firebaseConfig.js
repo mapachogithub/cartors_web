bf177dc299cb70d778703f72c9323126d220e6a8
mapachogithub <fraquiones11@misena.edu.co>
2025-05-15 16:36:27 -0500

    Certificaciones pendientes backend ok
    Certificaciones pendientes backend ok

---
+++ administracion/firebaseConfig.js
@@ -0,0 +1,21 @@
+// // firebaseConfig.js
+// export const firebaseConfig = {
+//   apiKey: "TU_API_KEY",
+//   authDomain: "TU_AUTH_DOMAIN",
+//   projectId: "TU_PROJECT_ID",
+//   storageBucket: "TU_STORAGE_BUCKET",
+//   messagingSenderId: "TU_MESSAGING_SENDER_ID",
+//   appId: "TU_APP_ID"
+// };
+
+
+// firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "TU_API_KEY", // Reemplaza con tu API Key real
+  authDomain: "TU_AUTH_DOMAIN", // Reemplaza con tu Auth Domain real
+  projectId: "cartors-sas-web", // **Este es el valor correcto**
+  storageBucket: "TU_STORAGE_BUCKET", // Reemplaza con tu Storage Bucket real
+  messagingSenderId: "TU_MESSAGING_SENDER_ID", // Reemplaza con tu Sender ID real
+  appId: "TU_APP_ID", // Reemplaza con tu App ID real
+  databaseURL: "https://cartors-sas-web-default-rtdb.firebaseio.com/" //Asegúrate de que esta URL sea correcta
+};
